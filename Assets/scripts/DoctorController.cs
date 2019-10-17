b67da740a815d110db03f261bf93f4b8b36ecd71
jhhtaylor <jhhtaylor@gmail.com>
2019-10-17 11:39:42 +0200

    Basic Enemy AI added to scene

---
+++ Assets/scripts/DoctorController.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+public class DoctorController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject player;
+
+    public NavMeshAgent agent;
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.gameObject.transform.position);
+    }
+}
