9a4ec24513d5fa871e8aaffa8a40ccda9043db4c
jhhtaylor <jhhtaylor@gmail.com>
2019-10-23 10:25:25 +0200

    Wake Up added

---
+++ Assets/scripts/WakeUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WakeUp : MonoBehaviour
+{
+    float timeLeft = 0.1f;
+    [SerializeField] private Image WakeUpImage;
+    [SerializeField] private Image IntroImage;
+    private float alphaLevel = 1f;
+    private int count = 100;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (count > 0)
+        {
+            if (timeLeft < 0)
+            {
+                alphaLevel -= 0.01f;
+                WakeUpImage.color = new Color(0, 0, 0, alphaLevel);
+                IntroImage.color = new Color(0, 0, 0, alphaLevel);
+                timeLeft = 0.1f;
+                count--;
+            }
+        }
+        else
+        {
+            IntroImage.gameObject.SetActive(false);
+        }
+    }
+}
