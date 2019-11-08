9a60cf9dd2d56923709c5157960d8761f3dcdc2d
jhhtaylor <jhhtaylor@gmail.com>
2019-11-08 15:39:06 +0200

    Added Ambient Sound

---
+++ Assets/scripts/AmbientSoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class AmbientSoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource AS;
+    [SerializeField] private float Vo = 1f;
+    [SerializeField] private float Vn = 0.3f;
+    [SerializeField] private float curr;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("LowerSound"))
+        {
+            StartCoroutine(dec());
+        }
+    }
+
+
+    // Once we exit colliding with a trigger collider
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("LowerSound"))
+        {
+            StartCoroutine(inc());
+        }
+    }
+
+
+    public IEnumerator dec()
+    {
+        while (curr >= Vn)
+        {
+            yield return new WaitForSeconds(0.2f);
+            AS.volume = curr;
+            curr -= 0.1f;
+        }
+
+        //curr = Vo;
+    }
+
+    public IEnumerator inc()
+    {
+        while (curr <= Vo)
+        {
+            yield return new WaitForSeconds(0.2f);
+            AS.volume = curr;
+            curr += 0.1f;
+        }
+
+        //curr = Vo;
+    }
+}
