ba6a781c1570faa69d4b8747d0486712f604a311
jhhtaylor <jhhtaylor@gmail.com>
2019-11-12 12:16:49 +0200

    Pause Menu Added

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("escape"))
+        if (Input.GetKeyDown("escape"))
         {
             P.pauseGame();
         }
